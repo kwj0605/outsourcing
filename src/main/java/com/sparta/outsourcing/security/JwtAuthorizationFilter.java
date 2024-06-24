64c720d1294eb3a8a4f15b19aacb219222901d7a
Delphinium52 <63048093+Delphinium52@users.noreply.github.com>
2024-06-24 16:27:40 +0900

    update : auth 관련 구현
    로그인 로그아웃 토큰재발급

--- src/main/java/com/sparta/outsourcing/security/JwtAuthorizationFilter.java
+++ src/main/java/com/sparta/outsourcing/security/JwtAuthorizationFilter.java
@@ -1,10 +1,12 @@
 package com.sparta.outsourcing.security;
 
+import com.sparta.outsourcing.enums.AuthEnum;
 import com.sparta.outsourcing.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -16,69 +18,48 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@RequiredArgsConstructor
 @Slf4j(topic = "JWT 검증 및 인가")
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
-    private final UserDetailsServiceImpl userDetailsService;
-    public static final String AUTHORIZATION_HEADER = "Authorization";
-    public static final String REFRESH_TOKEN_HEADER = "RefreshToken";
 
-    public JwtAuthorizationFilter(JwtService jwtService, UserDetailsServiceImpl userDetailsService) {
-        this.jwtService = jwtService;
-        this.userDetailsService = userDetailsService;
-    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String accessToken = jwtService.getTokenFromRequest(request, AUTHORIZATION_HEADER);
-        String refreshToken = jwtService.getTokenFromRequest(request, REFRESH_TOKEN_HEADER);
 
-        if (StringUtils.hasText(accessToken)) {
-            // JWT 토큰 substring
-            accessToken = jwtService.substringToken(accessToken);
-            log.info(accessToken);
+        /**
+         * 토큰정보추출
+         */
+        String token = resolveToken(request);
 
-            // accessToken 만료 & refreshToken 유효한 경우
-            // 새로운 accessToken 발급
-            if (!jwtService.validateToken(accessToken)) {
-                if (refreshToken != null && jwtService.validateToken(refreshToken)){
-                    String username = jwtService.extractUsername(refreshToken);
-                    String newAccessToken = jwtService.createToken(username);
-
-                    response.setHeader(AUTHORIZATION_HEADER, newAccessToken);
-                    jwtService.addJwtToCookie(newAccessToken, response, true);
-                } else {
-                    log.error("Token Error");
-                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                    return;
-                }
-            }
-
-            String username = jwtService.getUsernameFromRequest(request);
-
-            try {
-                setAuthentication(username);
-            } catch (Exception e) {
-                log.error(e.getMessage());
-                return;
-            }
+        /**
+         *  validateToken 으로 토큰 유효성 검사
+         *  토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서 SecurityContext 에 저장
+         */
+        if (token != null && jwtService.validateToken(token)) {
+            Authentication authentication = jwtService.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
         filterChain.doFilter(request, response);
     }
 
-    // 인증 처리
-    private void setAuthentication(String userId) {
-        SecurityContext context = SecurityContextHolder.createEmptyContext();
-        Authentication authentication = createAuthentication(userId);
-        context.setAuthentication(authentication);
-        SecurityContextHolder.setContext(context);
+    /**
+     * Request Header 에서 토큰 정보 추출
+     *
+     * @param request
+     * @return
+     */
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AuthEnum.ACCESS_TOKEN.getValue());
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(
+                AuthEnum.GRANT_TYPE.getValue())) {
+            return bearerToken.substring(7);
+        }
+        return null;
     }
 
-    // 인증 객체 생성
-    private Authentication createAuthentication(String userId) {
-        UserDetails userDetails = userDetailsService.loadUserByUsername(userId);
-        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-    }
+
+
 }
